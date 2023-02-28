ca35606498232f04b61b12e10afa2f2523023963
EC2 Default User <ec2-user@ip-172-31-53-15.ec2.internal>
2023-02-28 06:04:50 +0000

    aws-bastion first

---
+++ aws-vpc-sg-bastion/terraform-manifests/c7-general-variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    type = string
+    default =  "us-east-1"
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
+variable "business_division" {
+    type = string
+    default = "HR"
+}
---
+++ aws-vpc-sg-bastion/terraform-manifests/c7-versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
